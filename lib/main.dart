4a4cefe9e233f1fd312781a45ac918f1ebe112fb
thomnico <thomnico@gmail.com>
2022-01-01 23:25:15 +0100

    Make the mix with FutureBuild and graphql example Some nullsafety applied

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,31 @@ import 'package:provider/provider.dart';
 
 import './domain/user.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'constants.dart';
+
+void main() async {
+  // We're using HiveStore for persistence,
+  // so we need to initialize Hive.
+  await initHiveForFlutter();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    Future<User> getUserData() => UserPreferences().getUser();
+    final HttpLink httpLink = HttpLink(kParseApiUrl, defaultHeaders: {
+      'X-Parse-Application-Id': kParseApplicationId,
+      'X-Parse-Client-Key': kParseClientKey,
+      //      'X-Parse-Session-Token': 'r:8e26ccce46ec4650cc79e2b969c50674'
+    });
+
+    ValueNotifier<GraphQLClient> client = ValueNotifier(
+      GraphQLClient(
+        cache: GraphQLCache(store: HiveStore()),
+        link: httpLink,
+      ),
+    );
 
     return MultiProvider(
       providers: [
@@ -26,33 +42,86 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => UserProvider()),
       ],
       child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
+          home: GraphQLProvider(
+            child: MyHomePage(),
+            client: client,
           ),
-          home: FutureBuilder(
-              future: getUserData(),
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                  case ConnectionState.waiting:
-                    return CircularProgressIndicator();
-                  default:
-                    if (snapshot.hasError)
-                      return Text('Error: ${snapshot.error}');
-                    else if (snapshot.data.token == null)
-                      return Login();
-                    else
-                      UserPreferences().removeUser();
-                    return Welcome(user: snapshot.data);
-                }
-              }),
           routes: {
             '/dashboard': (context) => DashBoard(),
             '/login': (context) => Login(),
             '/register': (context) => Register(),
           }),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // UserData is using the shared preferences module
+  Future<User> getUserData() => UserPreferences().getUser();
+
+  String? name;
+  String? saveFormat;
+  String? objectId;
+
+  static String query = '''
+  query FindHero {
+    heroes{
+      count,
+      edges{
+        node{
+          name
+          height
+        }
+      }
+    }
+  }
+  ''';
+
+  ///THIS IS A SAMPLE FOR MAKING MUTABLE REQUEST
+
+  static String loginQuery = '''
+    mutation LogIn{
+      logIn(input: {
+        username: "test",
+        password: "testtest"
+      }){
+        viewer{
+          sessionToken
+          user { 
+           objectId
+           id
+           emailVerified
+          }
+        }
+      }
+    }
+    ''';
+
+/////// check https://blog.logrocket.com/using-graphql-with-flutter-a-tutorial-with-examples/
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: FutureBuilder(
+            future: getUserData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return CircularProgressIndicator();
+                default:
+                  if (snapshot.hasError)
+                    return Text('Error: ${snapshot.error}');
+                  else if (!snapshot.hasData) if (snapshot.data == null)
+                    return Login();
+                  else
+                    UserPreferences().removeUser();
+                  return Welcome(user: snapshot.data as User);
+              }
+            }));
   }
 }
