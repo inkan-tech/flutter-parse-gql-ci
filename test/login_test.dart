4ee45853346efb65f0d0ad7baddd77d734a04b26
HugoM38 <pv.hugom@gmail.com>
2022-09-19 12:16:09 +0200

    Mock Login (don't work)

--- test/login_test.dart
+++ test/login_test.dart
@@ -1,11 +1,17 @@
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:parse_gql_ci/constants.dart';
 import 'package:parse_gql_ci/domain/user.dart';
+import 'package:parse_gql_ci/pages/dashboard.dart';
 import 'package:parse_gql_ci/pages/login.dart';
+import 'package:parse_gql_ci/pages/logout.dart';
+import 'package:parse_gql_ci/pages/register.dart';
 import 'package:parse_gql_ci/providers/auth.dart';
+import 'package:parse_gql_ci/providers/user_provider.dart';
 import 'package:parse_gql_ci/util/shared_preference.dart';
+import 'package:parse_gql_ci/util/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:parse_gql_ci/graphql-configurator.dart';
 import '../lib/main.dart' as app;
@@ -24,6 +30,126 @@ class MockClient extends Mock implements GraphQLClient {
   void setCache(GraphQLCache graphQLCache) {
     cache = graphQLCache;
   }
+}
+
+class MockLogin extends Mock implements Login {
+
+  //Source : https://github.com/dart-lang/mockito/issues/228#issuecomment-578400083
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.debug}) {
+    return super.toString();
+  }
+
+    final formKey = GlobalKey<FormState>();
+
+  late String _username = "", _password = "";
+
+  Widget build(BuildContext context) {
+    MockAuth auth = Provider.of<MockAuth>(context);
+
+        final usernameField = TextFormField(
+      key: const ValueKey('UsernameField'),
+      autofocus: false,
+      //  validator: validateEmail,
+      onSaved: (value) => _username = value as String,
+      decoration: buildInputDecoration("Confirm password", Icons.person),
+    );
+
+    final passwordField = TextFormField(
+      key: const ValueKey('PasswordField'),
+      autofocus: false,
+      obscureText: true,
+      validator: (value) => value!.isEmpty ? "Please enter password" : null,
+      onSaved: (value) => _password = value as String,
+      decoration: buildInputDecoration("Confirm password", Icons.lock),
+    );
+
+    var loading = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const <Widget>[
+        CircularProgressIndicator(),
+        Text(" Authenticating ... Please wait")
+      ],
+    );
+
+    final forgotLabel = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        ElevatedButton(
+          child: const Text("Forgot password?",
+              style: TextStyle(fontWeight: FontWeight.w300)),
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/reset-password');
+          },
+        ),
+        ElevatedButton(
+          child: const Text("Sign up",
+              style: TextStyle(fontWeight: FontWeight.w300)),
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/register');
+          },
+        ),
+      ],
+    );
+
+    var doLogin = () {
+      final form = formKey.currentState;
+
+      if (form!.validate()) {
+        form.save();
+
+        final Future<Map<String, dynamic>> successfulLogin =
+            auth.login(_username, _password);
+
+        successfulLogin.then((response) {
+          if (response['status']) {
+            User user = response['user'];
+            Provider.of<UserProvider>(context, listen: false).setUser(user);
+            Navigator.pushReplacementNamed(context, '/dashboard');
+          } else {
+            Flushbar(
+              title: "Failed Login",
+              message: response['message'].toString(),
+              duration: const Duration(seconds: 3),
+            ).show(context);
+          }
+        });
+      } else {
+        print("form is invalid");
+      }
+    };
+
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.all(40.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 15.0),
+                label("Username"),
+                const SizedBox(height: 5.0),
+                usernameField,
+                const SizedBox(height: 20.0),
+                label("Password"),
+                const SizedBox(height: 5.0),
+                passwordField,
+                const SizedBox(height: 20.0),
+                auth.loggedInStatus == Status.Authenticating
+                    ? loading
+                    : longButtons("Login", doLogin),
+                const SizedBox(height: 5.0),
+                forgotLabel
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
 }
 
 class MockLink extends Mock implements HttpLink {}
@@ -72,7 +198,7 @@ class MockAuth extends Mock implements AuthProvider {
         "logIn": {
           "viewer": {
             'user': {
-              "objectId": "aaabbb",
+              "objectId": "test",
               "id": "bbbbbbbbbccccc",
               "emailVerified": true,
               "email": "thomnico+b4p@gmail.com"
@@ -132,6 +258,8 @@ void main() {
 
   MockClient mockClient = MockClient();
 
+  MockAuth mockAuth = MockAuth();
+
   mockClient.setCache(GraphQLCache());
   mockClient.setLink(mockLink);
 
@@ -139,14 +267,22 @@ void main() {
 
   testWidgets('Test the login / logout ', (WidgetTester tester) async {
     await tester.pumpWidget(Builder(
-        builder: ((context) => MaterialApp(
-                home: GraphQLProvider(
+        builder: ((context) => GraphQLProvider(
               client: client,
-              child: ChangeNotifierProvider<AuthProvider>(
-                create: (_) => MockAuth(),
-                child: const Login(),
+              child: ChangeNotifierProvider<MockAuth>(
+                create: (_) => mockAuth,
+                builder: (context, child) => MaterialApp(
+                  home: MockLogin(),
+                  routes: {
+                    '/dashboard': (context) => const DashBoard(),
+                    '/login': (context) => const Login(),
+                    '/register': (context) => const Register(),
+                    '/logout': (context) => Logout(),
+                  },
+                ),
+                //child: const Login(),
               ),
-            )))));
+            ))));
 
     await tester.enterText(find.byKey(const ValueKey('UsernameField')), 'test');
 
